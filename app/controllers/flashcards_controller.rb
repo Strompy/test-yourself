47e3a78d31c92160f2bae7c22a58a34f122b5456
Phillip Strom <phillip.strom@gmail.com>
2024-05-24 10:47:48 -0600

    Create flashcard new and create actions

--- app/controllers/flashcards_controller.rb
+++ app/controllers/flashcards_controller.rb
@@ -2,5 +2,24 @@ class FlashcardsController < ApplicationController
     def index
         @flashcards = Flashcard.all
     end
+    
+    def new
+        @flashcard = Flashcard.new
+    end
 
+    def create
+        @flashcard = Flashcard.new(flashcard_params)
+
+        if @flashcard.save
+            redirect_to flashcards_path, notice: 'Flashcard created successfully'
+        else
+            render :new, notice: "Flashcard not created: #{flashcard.errors.full_messages.to_sentence}"
+        end
+    end
+
+    private
+
+    def flashcard_params
+        params.require(:flashcard).permit(:question, :answer)
+    end
 end
