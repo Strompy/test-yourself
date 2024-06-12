13c36e673dce123b06b92a2275d8502acff7495c
Phillip Strom <phillip.strom@gmail.com>
2024-06-12 10:10:32 -0600

    Create searches#show

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,9 @@
+class SearchesController < ApplicationController
+    def show
+        if params[:query].present?
+            @flashcards = Flashcard.where("question like ?", "%#{params[:query]}%").limit(5)
+        else
+            @flashcards = Flashcard.all.limit(5)
+        end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
 
   root "flashcards#index"
   resources :flashcards
+  resource :search, only: :show
 end
