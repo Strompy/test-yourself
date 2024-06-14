517959983e6e69a9e6af6d398b33b1a8201e1596
Phillip Strom <phillip.strom@gmail.com>
2024-06-14 11:05:42 -0600

    Add some ksearch styling

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,7 @@
 class SearchesController < ApplicationController
     def show
         if params[:query].present?
-            @flashcards = Flashcard.where("question like ?", "%#{params[:query]}%").limit(5)
+            @flashcards = Flashcard.where("question ilike ?", "%#{params[:query]}%").limit(5)
         else
             @flashcards = Flashcard.all.limit(5)
         end
